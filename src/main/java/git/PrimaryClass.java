b1f033bdf1e0bcf7c5e3a0a83b4c67a808ace2da
Mayank Srivastava <mayank.srivastava@jci.com>
2019-05-18 23:24:19 +0530

    commited

---
+++ src/main/java/git/PrimaryClass.java
@@ -0,0 +1,13 @@
+package git;
+
+import org.testng.annotations.Test;
+
+public class PrimaryClass {
+	
+	
+	@Test
+	public void PrintMessage() {
+		System.out.println("This is message from PrintMessage class.");
+	}
+
+}
